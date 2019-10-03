2eb2253dd887eda8ad77317609817ac9b5e45841
Jamaladdin Bahmani <43005370+jabahmani@users.noreply.github.com>
2019-10-03 12:15:35 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once 'Request.php';
+include_once 'Router.php';
+$router = new Router(new Request);
+
+$router->get('/', function()
+{
+	return "<h1>Hello world</h1>";
+});
+
+$router->get('/profile', function($request)
+{
+	return "<h1>Profile</h1>";
+});
+
+$router->post('/data', function($request)
+{
+  return json_encode($request->getBody());
+});
