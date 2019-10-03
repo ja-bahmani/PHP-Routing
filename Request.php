ad1d5398c50452dcea39c75521907038505b23e4
Jamaladdin Bahmani <43005370+jabahmani@users.noreply.github.com>
2019-10-03 12:19:38 +0330

    Update Request.php

--- Request.php
+++ Request.php
@@ -1,54 +1,54 @@
 <?php
-include_once 'IRequest.php';
+interface IRequest
+{
+	public function getBody();
+}
 
 class Request implements IRequest
 {
-  function __construct()
-  {
-    $this->bootstrapSelf();
-  }
+	function __construct()
+	{
+		$this->bootstrapSelf();
+	}
 
-  private function bootstrapSelf()
-  {
-    foreach($_SERVER as $key => $value)
-    {
-      $this->{$this->toCamelCase($key)} = $value;
-    }
-  }
+	private function bootstrapSelf()
+	{
+		foreach($_SERVER as $key => $value)
+		{
+			$this->{$this->toCamelCase($key)} = $value;
+		}
+	}
 
-  private function toCamelCase($string)
-  {
-    $result = strtolower($string);
+	private function toCamelCase($string)
+	{
+		$result = strtolower($string);
         
-    preg_match_all('/_[a-z]/', $result, $matches);
+		preg_match_all('/_[a-z]/', $result, $matches);
 
-    foreach($matches[0] as $match)
-    {
-        $c = str_replace('_', '', strtoupper($match));
-        $result = str_replace($match, $c, $result);
-    }
+		foreach($matches[0] as $match)
+		{
+			$c = str_replace('_', '', strtoupper($match));
+			$result = str_replace($match, $c, $result);
+		}
+		return $result;
+	}
 
-    return $result;
-  }
-
-  public function getBody()
-  {
-    if($this->requestMethod === "GET")
-    {
-      return;
-    }
+	public function getBody()
+	{
+		if($this->requestMethod === "GET")
+		{
+			return;
+		}
 
 
-    if ($this->requestMethod == "POST")
-    {
-
-      $body = array();
-      foreach($_POST as $key => $value)
-      {
-        $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
-      }
-
-      return $body;
-    }
-  }
+		if ($this->requestMethod == "POST")
+		{
+			$body = array();
+			foreach($_POST as $key => $value)
+			{
+				$body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+			}
+			return $body;
+		}
+	}
 }
